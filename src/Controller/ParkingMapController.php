b3c9785bdf585b6aaafe988f00b5a3ce24f61865
Johanna Galli <galli.johanna.g2@gmail.com>
2020-04-03 17:00:55 +0200

    insertion de la parking-map

--- src/Controller/ParkingMapController.php
+++ src/Controller/ParkingMapController.php
@@ -13,25 +13,51 @@ class ParkingMapController extends AbstractController
      */
     public function index()
     {
+        //-1-RECUPERATION DU NOMBRE DE PARKINGS
         $client = HttpClient::create();
-        $response = $client->request('GET', 'https://data.strasbourg.eu/api/records/1.0/search/?dataset=parkings');
-
-        $statusCode = $response->getStatusCode();
-        // $statusCode = 200
-        $contentType = $response->getHeaders()['content-type'][0];
-        // $contentType = 'application/json'
-        $content1 = $response->getContent();
-        // $content = '{"id":521583, "name":"symfony-docs", ...}'
-        $content2 = $response->toArray();
+        $response = $client->request('GET', 'https://data.strasbourg.eu/api/records/1.0/search/?dataset=parkings&rows=0');
+        $content = $response->toArray();
         // $content = ['id' => 521583, 'name' => 'symfony-docs', ...]
-        $startTime = $response->getInfo('start_time');
+        $nhits = $content['nhits'];
+        // nhits = 29 (nbr de parkings)
 
+
+        //-2-INSERTION DU NB DE PARKINGS DANS URL
+        $response = $client->request('GET', "https://data.strasbourg.eu/api/records/1.0/search/?dataset=parkings&rows=$nhits");
+        $content = $response->getContent();
+        // $content = '{"id":521583, "name":"symfony-docs", ...}'
+
+
+        //-3-RECUPERATION DES DONNÉES 
+        $contentArray = $response->toArray();
+        // $contentArray = ['id' => 521583, 'name' => 'symfony-docs', ...]
+        $records = $contentArray["records"];
+        for ($i = 0; $i < $nhits; $i++) {
+            $parkings = $records[$i];
+            $fields = $parkings['fields'];
+            $position = var_dump($fields['position']);
+
+            /*
+            while ($parkings) {
+                $fields = $parkings['fields'];
+
+                foreach ($fields as $key => $value) {
+
+                    $infosParking = var_dump('clé : "' . $key . '" valeur : "' . $value . '"<br />');
+                    
+                $position = $fields['position'];
+                $donneePosition = $position[$i];
+                }
+            }*/
+        }
+
+
+
+        //--ENVOI
         return $this->render('parking_map/index.html.twig', [
             'controller_name' => 'ParkingMapController',
-            'statusCode' => $statusCode,
-            'contentType' => $contentType,
-            'content1' => $content1,
-            'startTime' => $startTime
+            'content' => $content,
+            '$position' => $position
         ]);
     }
 }
