2931569e538fe13311878e2cf86fda2f2832db44
Johanna Galli <galli.johanna.g2@gmail.com>
2020-04-08 11:05:13 +0200

    map fonctionnel avec tous parkings

--- src/Controller/ParkingMapController.php
+++ src/Controller/ParkingMapController.php
@@ -6,12 +6,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpClient\HttpClient;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
+
 class ParkingMapController extends AbstractController
 {
     /**
      * @Route("/parking/map", name="parking_map")
      */
     public function index()
+    {
+        return $this->render('parking_map/index.html.twig', [
+            'controller_name' => 'ParkingMapController'
+        ]);
+    }
+
+    
+    /**
+     * @Route("/parking/mapData", name="parking_mapData")
+     */
+    public function getData()
     {
         //-1-RECUPERATION DU NOMBRE DE PARKINGS
         $client = HttpClient::create();
@@ -32,32 +45,38 @@ class ParkingMapController extends AbstractController
         $contentArray = $response->toArray();
         // $contentArray = ['id' => 521583, 'name' => 'symfony-docs', ...]
         $records = $contentArray["records"];
+
         for ($i = 0; $i < $nhits; $i++) {
             $parkings = $records[$i];
             $fields = $parkings['fields'];
-            $position = var_dump($fields['position']);
 
-            /*
-            while ($parkings) {
-                $fields = $parkings['fields'];
+            //nom du parking
+            $name = $fields['name'];
+            //var_dump($name);
 
-                foreach ($fields as $key => $value) {
+            //adresse 
+            $address = $fields['address'];
 
-                    $infosParking = var_dump('clé : "' . $key . '" valeur : "' . $value . '"<br />');
-                    
-                $position = $fields['position'];
-                $donneePosition = $position[$i];
-                }
-            }*/
+            //position géographique
+            $position = $fields['position'];
+            //$position retourne un tableau avec index 0 : axe hotizontal et index 1 axe vertical
+
+            $idsurfs = $fields['idsurfs'];
+
+            $infoParking[$i] = [
+                'name' => $name,
+                'address' => $address,
+                'positionX' => $position[0],
+                'positionY' => $position[1],
+                'idsurfs' => $idsurfs
+            ];
         }
 
+        /*echo "<pre>";
+        var_dump($infoParking);
+        echo "</pre>";*/
 
-
-        //--ENVOI
-        return $this->render('parking_map/index.html.twig', [
-            'controller_name' => 'ParkingMapController',
-            'content' => $content,
-            '$position' => $position
-        ]);
+        $response = new JsonResponse($infoParking);
+        return $response;
     }
 }
