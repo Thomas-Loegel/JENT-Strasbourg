35d3d9b398b14779111583dd0c8e8c44ba33a800
Mineli <monte.nemi@gmail.com>
2020-04-21 10:31:31 +0200

    added VeloMapController.php

--- src/Controller/VeloMapController.php
+++ src/Controller/VeloMapController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class VeloMapController extends AbstractController
 {
@@ -16,4 +18,32 @@ class VeloMapController extends AbstractController
             'controller_name' => 'VeloMapController',
         ]);
     }
+
+    /**
+     * @Route("/velo/velosdata", name="velosdata")
+     */
+    public function getData(HttpClientInterface $httpClient) {
+      $response = $httpClient->request('GET', 'https://data.strasbourg.eu/api/records/1.0/search/?dataset=stations-velhop&rows=21&facet=na');
+
+        $data = $response->toArray()['records'];
+
+        $array = [];
+
+        foreach($data as $records) {
+          $array[$records['recordid']]['na'] = $records['fields']['na'];
+          $array[$records['recordid']]['coordonnees'] = $records['fields']['coordonnees'];
+          $array[$records['recordid']]['to'] = $records['fields']['to'];
+          $array[$records['recordid']]['av'] = $records['fields']['av'];
+          $array[$records['recordid']]['fr'] = $records['fields']['fr'];
+        }
+
+        //dd($array);
+
+        $velosData = new JsonResponse($array);
+
+        //dd($velosData);
+        
+        return $velosData;
+    }
+}
 }
